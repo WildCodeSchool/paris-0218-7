cbab15b7650cd7b2290731c98ca6bcdec509b47a
Edouard de Laage <edouarddelaage@gmail.com>
2018-03-29 20:18:55 +0200

    inject alumnis innerHTML

--- client/js/app.js
+++ client/js/app.js
@@ -1,8 +1,6 @@
 fetch('http://localhost:3248/alumnis')
 	.then(response => response.json())
 	.then(alumnis => {
-
-
-
-		console.log(alumnis)
+		const alumnisElement = document.getElementById('alumnis')
+		alumnisElement.innerHTML = JSON.stringify(alumnis)
 	})
