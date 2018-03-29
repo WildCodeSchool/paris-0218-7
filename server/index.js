badef837561412ebe9bf81b76af65a81a7936d57
Edouard de Laage <edouarddelaage@gmail.com>
2018-03-29 18:40:10 +0200

    Loads mocks alumnis

--- server/index.js
+++ server/index.js
@@ -1,4 +1,19 @@
 const express = require('express')
+const alumni1 = require('../mocks/alumnis/alumni1.json')
+const alumni2 = require('../mocks/alumnis/alumni2.json')
+const alumni3 = require('../mocks/alumnis/alumni3.json')
+const alumni4 = require('../mocks/alumnis/alumni4.json')
+const alumni5 = require('../mocks/alumnis/alumni5.json')
+
+const allAlumnis = [
+	alumni1,
+	alumni2,
+	alumni3,
+	alumni4,
+	alumni5
+]
+
+console.log(allAlumnis)
 
 const app = express()
 
