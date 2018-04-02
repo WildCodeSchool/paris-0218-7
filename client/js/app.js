3dbf25c7e1e5473d1bfe0cba3d90a6f87e119a39
Edouard de Laage <edouarddelaage@gmail.com>
2018-04-02 15:39:13 +0200

    Function to calculate the age of an alumnis

--- client/js/app.js
+++ client/js/app.js
@@ -1,11 +1,39 @@
+const _calculateAge = (birthday) =>{ // birthday is a date
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+
+
 const showAlumnis = (alumni) => {
-	var img = new Image();	
+
+	//Date de naissance
+	const birth = new Date(alumni.birthDate)
+	// let day = birth.getDate()
+	// let month = birth.getMonth()+1
+	// const year = birth.getFullYear()
+	// if (month < 10) {
+	// 	month = (`0${month}`)
+	// }
+	// if(day < 10){
+	// 	day = (`0${day}`)
+	// }
+	// const dateBirth = (`${day}/${month}/${year}`)
+
+	// //age
+	// const date = new Date()
+	// const age = date - birth
+	// console.log(_calculateAge(birth))
+
+
+	const img = new Image();	
 	img.src = alumni.img
 		return `
 		<div class="liste_alumnis">
 			<img src="${alumni.img}" id="image">
 			<h4 id="prenom">${alumni.firstName} ${alumni.lastName}</h4>
-			<p id="birthday">${alumni.birthDate}</p>
+			<p id="birthday">${_calculateAge(birth)} ans</p>
 			<p id="campus">Campus: ${alumni.campus}</p>
 		</div>`
 }
