99a544aa3675800253400a37250737583b0423f1
Edouard de Laage <edouarddelaage@gmail.com>
2018-04-02 12:40:20 +0200

    Added Adding the function to display the number of members

--- client/js/app.js
+++ client/js/app.js
@@ -18,4 +18,9 @@ fetch('http://localhost:3248/alumnis')
 		const alumnisElement = document.getElementById('block_alumnis')
 		const alumniElements = personne.map(showAlumnis).join(' ')
 		alumnisElement.innerHTML = alumniElements
+
+		const nbElement = document.getElementById('nb_alumni')
+		nbElement.innerHTML = (`
+				<p>${personne.length} membres correspondent à votre recherche</p>
+			`)
 	})
