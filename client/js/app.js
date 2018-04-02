ab8c906e95220d88db38f0bbc9c6906f947c88fc
Edouard de Laage <edouarddelaage@gmail.com>
2018-04-02 12:34:29 +0200

    Added function showAlumnis in app.js / added img profile file / added link img profile in mocks

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,21 @@
+const showAlumnis = (alumni) => {
+	var img = new Image();	
+	img.src = alumni.img
+		return `
+		<div class="liste_alumnis">
+			<img src="${alumni.img}" id="image">
+			<h4 id="prenom">${alumni.firstName} ${alumni.lastName}</h4>
+			<p id="birthday">${alumni.birthDate}</p>
+			<p id="campus">Campus: ${alumni.campus}</p>
+		</div>`
+}
+
+
+
 fetch('http://localhost:3248/alumnis')
 	.then(response => response.json())
-	.then(alumnis => {
-		const alumnisElement = document.getElementById('alumnis')
-		alumnisElement.innerHTML = JSON.stringify(alumnis)
+	.then(personne => {
+		const alumnisElement = document.getElementById('block_alumnis')
+		const alumniElements = personne.map(showAlumnis).join(' ')
+		alumnisElement.innerHTML = alumniElements
 	})
