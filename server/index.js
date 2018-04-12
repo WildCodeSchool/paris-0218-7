5a8f3cc0c649f9b6ab8d0ccb55819cd9fac17898
Edouard de Laage <edouarddelaage@gmail.com>
2018-04-12 16:00:22 +0200

    Display on index.html all alumnis with FS method and promise and delete of require('../mocks/alumnis/alumnis1.json) and allAlumnis

--- server/index.js
+++ server/index.js
@@ -4,24 +4,25 @@ const util = require('util')
 const path = require('path')
 
 const readFile = util.promisify(fs.readFile)
+const readdir = util.promisify(fs.readdir)
 
 
-const alumni1 = require('../mocks/alumnis/alumni1.json')
-const alumni2 = require('../mocks/alumnis/alumni2.json')
-const alumni3 = require('../mocks/alumnis/alumni3.json')
-const alumni4 = require('../mocks/alumnis/alumni4.json')
-const alumni5 = require('../mocks/alumnis/alumni5.json')
+// const alumni1 = require('../mocks/alumnis/alumni1.json')
+// const alumni2 = require('../mocks/alumnis/alumni2.json')
+// const alumni3 = require('../mocks/alumnis/alumni3.json')
+// const alumni4 = require('../mocks/alumnis/alumni4.json')
+// const alumni5 = require('../mocks/alumnis/alumni5.json')
 
-const allAlumnis = [
-	alumni1,
-	alumni2,
-	alumni3,
-	alumni4,
-	alumni5,
-	alumni1,
-	alumni2,
-	alumni3
-]
+// const allAlumnis = [
+// 	alumni1,
+// 	alumni2,
+// 	alumni3,
+// 	alumni4,
+// 	alumni5,
+// 	alumni1,
+// 	alumni2,
+// 	alumni3
+// ]
 
 const app = express()
 
@@ -36,6 +37,41 @@ app.use((request, response, next) => {
 app.get('/', (request, response) => {
 	response.end('ok')
 })
+//
+//
+//
+//
+//
+//
+//
+//For display all alumnis on index.html page with require at the top of this page
+app.get('/alumnis', (request, response) => {
+	const alumniDirr = (path.join(__dirname, '../mocks/alumnis'))
+	// console.log(alumniDirr)
+	readdir(alumniDirr)
+		.then(files => {
+			const filePaths = files.map(file => path.join(alumniDirr, file))
+			const allFiles = filePaths.map(filePath => {
+				return readFile(filePath, 'utf-8') 
+			})
+			Promise.all(allFiles)
+				.then(allFilesValues => {
+				// console.log(allFilesValues)
+				const valueInJason = allFilesValues.map(JSON.parse)
+				// console.log(valueInJason)
+				response.json(valueInJason)
+			})
+			.catch(err => {
+				response.status(500).end(err.message)
+			})
+		})
+	})
+//
+//
+//
+//
+//
+//
 
 //Find the ID for display the profile detail with read file 
 app.get('/alumnis/:id', (request, response) => {
@@ -70,12 +106,14 @@ app.get('/alumnis/:id', (request, response) => {
 	// const id = Number(request.params.id)
 	// const alumni = allAlumnis.find(alumni => alumni.id === id)
 	// response.json(alumni)
+//
+//
+//
+//
+//
 
 
 
-//For display all alumnis on index.html page with require at th etop of this page
-app.get('/alumnis', (request, response) => {
-	response.json(allAlumnis)
-})
+
 
 app.listen(3248, () => console.log('J\'écoute sur le port 3248'))
