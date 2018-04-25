318f61abe437ca29d35cad192f0910882a6b4a97
jeffonmac <cjoffrey@gmail.com>
2018-04-25 14:36:45 +0200

    firstNameSearch

--- client/js/app.js
+++ client/js/app.js
@@ -1,15 +1,26 @@
 import { showAlumnis } from './components/showAlumni.js'
 
+const renderAlumnis = alumnis => {
+  const alumnisElement = document.getElementById('block_alumnis')
+  const alumniElements = alumnis.map(showAlumnis).join('')
+  alumnisElement.innerHTML = alumniElements
+  console.log(alumniElements)
+
+  const nbElement = document.getElementById('nb_alumni')
+  nbElement.innerHTML = `
+    <p>${alumnis.length} membres correspondent à votre recherche</p>
+  `
+}
+
 fetch('http://localhost:3248/alumnis')
   .then(response => response.json())
-  .then(personne => {
-    const alumnisElement = document.getElementById('block_alumnis')
-    const alumniElements = personne.map(showAlumnis).join('')
-    alumnisElement.innerHTML = alumniElements
-    console.log(alumniElements)
+  .then(alumnis => {
+    const input = document.getElementById('search')
 
-    const nbElement = document.getElementById('nb_alumni')
-    nbElement.innerHTML = `
-      <p>${personne.length} membres correspondent à votre recherche</p>
-    `
+    input.addEventListener('input', event => {
+        const value = event.target.value
+        renderAlumnis(alumnis.filter(alumni => alumni.firstName.toLowerCase().includes(value)))
+    })
+
+    renderAlumnis(alumnis)
   })
