8f7210e37830806ce11d7ec9d80bbe9ea747f843
jeffonmac <cjoffrey@gmail.com>
2018-04-26 10:10:35 +0200

    searchValuesGood

--- client/js/app.js
+++ client/js/app.js
@@ -1,24 +1,8 @@
 import { showAlumnis } from './components/showAlumni.js'
 
-const searchableKeys = [ 
-  "firstName",
-  "lastName",
-  "birthDate",
-  "campus",
-  "dateSession",
-  "specialization",
-  "language"
-]
-const searchableValues = p => searchableKeys.map(k => p[k])
 const renderAlumnis = alumnis => {
-  
-  // Creating an alumni form
   const alumnisElement = document.getElementById('block_alumnis')
-  
-  // Creation of all alumni files
   const alumniElements = alumnis.map(showAlumnis).join('')
-  
-
   alumnisElement.innerHTML = alumniElements
 
   const nbElement = document.getElementById('nb_alumni')
@@ -33,13 +17,30 @@ fetch('http://localhost:3248/alumnis')
     const input = document.getElementById('search')
 
     renderAlumnis(alumnis)
-    alumnis.forEach(alumni => alumni.element = document.getElementById(`alumni-${alumni.id}`))  
 
     input.addEventListener('input', event => {
-      const search = event.target.value.toLowerCase()
-      const includeSearch = value => String(value).toLowerCase().includes(search)
+      const value = event.target.value.toLowerCase()
+        
+      const alumnisTab = [ 
+        "firstName",
+        "lastName",
+        "campus",
+        "dateSession",
+        "specialization"
+      
+      ]
 
-      alumnis.forEach(p => p.element.style.display = searchableValues(p).some(includeSearch) ? 'inherit' : 'none')
+      const checkIfFound = (alumni) => {
+        for ( let i = 0 ; i < alumnisTab.length ; i++ ) {
+          if (alumni[alumnisTab[i]].toLowerCase().includes(value) === true) {
+            return true
+          }
+        }
+        return false
+      }
+      
+      renderAlumnis(alumnis.filter(p => checkIfFound(p)))
     })
   })
+  
 
