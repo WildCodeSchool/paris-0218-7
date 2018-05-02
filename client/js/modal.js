ac92b6994c904955b5a8a12253e028fceb45188f
Sichelle Chiffert <sichelle.chiffert@gmail.com>
2018-05-02 16:12:41 +0200

    modal open/close handling

--- client/js/modal.js
+++ client/js/modal.js
@@ -1,25 +1,25 @@
 // Get the modal
-let modal = document.getElementById('myModal');
+const modal = document.getElementById('myModal')
 
 // Get the button that opens the modal
-let btn = document.getElementById("myBtn");
+const btn = document.getElementById('btn-modifier')
 
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName('close')[0]
 
 // When the user clicks the button, open the modal
 btn.onclick = () => {
-    modal.style.display = "block";
+  modal.style.display = 'block'
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = () => {
-    modal.style.display = "none";
+  modal.style.display = 'none'
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = (event) => {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+  if (event.target == modal) {
+    modal.style.display = 'none'
+  }
 }
